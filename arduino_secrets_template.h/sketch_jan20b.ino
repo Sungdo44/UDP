88595edfcb498edbb64ee27febf7573c04a2cb34
Sungdo44 <167730246+Sungdo44@users.noreply.github.com>
2026-01-20 10:57:29 +0700

    Add files via upload

---
+++ arduino_secrets_template.h/sketch_jan20b.ino
@@ -0,0 +1,4 @@
+// arduino_secrets_template.h
+// ให้เปลี่ยนชื่อไฟล์นี้เป็น arduino_secrets.h และใส่ IP จริงก่อนใช้งาน
+#define SECRET_SERVER_IP "YOUR_IP_ADDRESS_HERE"
+#define SECRET_SERVER_PORT 0000
